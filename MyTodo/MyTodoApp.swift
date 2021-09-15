f09cb878524d4c8e45af4dd4de8398cd52cb7353
meiichin <meiichan3017@gmail.com>
2021-09-16 00:47:36 +0700

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by Yuu on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
